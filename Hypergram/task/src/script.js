f847628d4a04995195003ae097ef6c7e666363ca
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-06-04 12:25:54 +0200

    Stage 2/4 class constructor solution

--- Hypergram/task/src/script.js
+++ Hypergram/task/src/script.js
@@ -1,19 +1,34 @@
-let fileInput = document.getElementById("file-input");
-fileInput.addEventListener('change', (ev) => {
-    if (ev.target.files) {
-        let file = ev.target.files[0];
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = (e) => {
-            let image = new Image();
-            image.src = e.target.result;
-            image.onload = (ev) => {
-                let canvas = document.getElementById("canvas");
-                canvas.width = image.width;
-                canvas.height = image.height;
-                let ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0);
-            }
+class App {
+    constructor(htmlEl) {
+        this.canvas = htmlEl.querySelector("canvas");
+        this.context = this.canvas.getContext("2d");
+        this.fileInputEl = htmlEl.querySelector("#file-input");
+        this.brightnessInputEl = htmlEl.querySelector("#brightness");
+        this.contrastInputEl = htmlEl.querySelector("#contrast");
+        this.transparentInputEl = htmlEl.querySelector("#transparent");
+        this.saveBtn = htmlEl.querySelector("#save-button");
+
+        this.fileInputEl.addEventListener("input", this.handleUpload.bind(this));
+        this.initialize();
+    }
+    initialize() {
+        this.canvas.width = 30;
+        this.canvas.height = 30;
+    }
+    handleUpload() {
+        const file = this.fileInputEl.files[0];
+
+        const imgEl = document.createElement("img");
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file)
+        reader.onload = (e) => {
+            imgEl.src = reader.result;
+            imgEl.onload = () => {
+                this.context.drawImage(imgEl, 0, 0)
+            };
         }
     }
-})
+}
+
+const app = new App(document.body);
